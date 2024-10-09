7daf7a852a5b6547dbd562bad72a4ffbc86bd77e
s-motto <brinamotto@gmail.com>
2024-10-09 17:45:23 +0200

    added webjars

---
+++ src/test/java/org/milestone/platform/ticket/TicketPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package org.milestone.platform.ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
